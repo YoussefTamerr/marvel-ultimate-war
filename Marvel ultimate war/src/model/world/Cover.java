8b3147578c136e3052f14adfa82bb9ba99476726
YoussefTamerr <yousseftamer1990@gmail.com>
2022-03-22 15:12:37 +0200

    points 9,10,11

--- Marvel ultimate war/src/model/world/Cover.java
+++ Marvel ultimate war/src/model/world/Cover.java
@@ -3,12 +3,34 @@ import java.awt.Point;
 
 public class Cover {
 	
-	int currentHP;
-	Point location;
+	private int currentHP;
+	private Point location;
 	
 	public Cover(int x, int y)
 	{
+		int min = 100;
+		int max = 999;
+		
+		this.currentHP= (int)Math.floor(Math.random()*(max-min+1)+min);
+		location = new Point(x,y);
 		
 	}
-	
+
+	public int getCurrentHP() {
+		return currentHP;
+	}
+
+	public void setCurrentHP(int currentHP) {
+		this.currentHP = currentHP;
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+	public static void main(String[] args) {
+		Cover c = new Cover(7,9);
+		Point l = c.getLocation();
+		double k = l.getX();
+		System.out.println(k);
+	}
 }
